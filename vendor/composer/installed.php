a1e1e8efed540835c4029419ace70344d8a7938e
Dan Richards <danrichardsri@gmail.com>
2026-04-14 01:56:22 -0400

    Tweak' g

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'dan/ai-loom-planner',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => null,
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '1e729354c33668842b76710ffa31c8a9f669f895',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -44,9 +44,9 @@
             ),
         ),
         'dan/ai-loom-planner' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => null,
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '1e729354c33668842b76710ffa31c8a9f669f895',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
